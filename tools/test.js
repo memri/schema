f8dee45d4e0d6c49bfc1b0d1ef3e58746462a869
rubenseggers <rubenseggers@gmail.com>
2020-07-21 14:47:14 +0200

    check for unused items and edges

--- tools/test.js
+++ tools/test.js
@@ -45,15 +45,39 @@ let predicateHierarchy = {};
   console.log('\nCheck for duplicate properties and edges (inherited and redefined)...');
   for (const entity of Object.keys(entityHierarchy)) {
     if (entityHierarchy[entity]['children']) {
-      const fields = entityHierarchy[entity]['properties'].concat(Object.keys(entityHierarchy[entity]['relations']))
+      const fields = entityHierarchy[entity]['properties'].concat(Object.keys(entityHierarchy[entity]['relations']));
       for (const child of entityHierarchy[entity]['children']) {
-        const childFields = entityHierarchy[child]['properties'].concat(Object.keys(entityHierarchy[child]['relations']))
+        const childFields = entityHierarchy[child]['properties'].concat(Object.keys(entityHierarchy[child]['relations']));
         for (const childField of childFields) {
           if (fields.includes(childField)) {
-            console.log(`E: ${child} redefines ${childField} that is already in ${entity}.`)
+            console.log(`E: ${child} redefines ${childField} that is already in ${entity}.`);
           }
         }
       }
+    }
+  }
+
+  console.log('\nCheck for unused Items...');
+  let usedTypes = new Set();
+  for (const predicate of Object.values(predicateHierarchy)) {
+    if (predicate['type']) usedTypes.add(predicate['type']);
+  }
+  for (const entity of Object.keys(entityHierarchy)) {
+    if (!usedTypes.has(entity)) {
+      console.log(`W: No Edge uses Item ${entity}`);
+    }
+  }
+
+  console.log('\nCheck for unused Edges...');
+  let usedEdges = new Set();
+  for (const entity of Object.values(entityHierarchy)) {
+    for (const relation of Object.keys(entity['relations'])) {
+      usedEdges.add(relation);
+    }
+  }
+  for (const edge of Object.keys(predicateHierarchy)) {
+    if (!(usedEdges.has(edge) || helpers.PRIMITIVE_TYPES.includes(predicateHierarchy[edge]['type']))) {
+      console.log(`W: No Item uses Edge ${edge}`);
     }
   }
 
@@ -61,24 +85,21 @@ let predicateHierarchy = {};
   for (const entity of Object.keys(entityHierarchy)) {
     if (entityHierarchy[entity]['description']) {
       if (entityHierarchy[entity]['description'].toLowerCase().includes('tbd')) {
-        console.log(`W: Item ${entity} has TBD in description.`)
+        console.log(`W: Item ${entity} has TBD in description.`);
       }
     } else {
-      console.log(`W: Item ${entity} is missing a description.`)
+      console.log(`W: Item ${entity} is missing a description.`);
     }
   }
   for (const predicate of Object.keys(predicateHierarchy)) {
     if (predicateHierarchy[predicate]['description']) {
       if (predicateHierarchy[predicate]['description'].toLowerCase().includes('tbd')) {
-        console.log(`W: Edge / Property ${predicate} has TBD in description.`)
+        console.log(`W: Edge / Property ${predicate} has TBD in description.`);
       }
     } else {
-      console.log(`W: Edge / Property ${predicate} is missing a description.`)
+      console.log(`W: Edge / Property ${predicate} is missing a description.`);
     }
   }
 
-  // TODO check if lower cased properties and relations don't clash: they can't have different types
-  // TODO check if there are unused Items
-  // TODO check if there are unused relationships/properties
   // TODO check if properties are shared over all children of an Item, so they could be inherited
 })();
